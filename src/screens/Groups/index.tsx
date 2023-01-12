4bd4e29a713db6efc8cce9fac50eaf9cc93cf489
Ryan Henrique de Assis Belfort <ryanhenriqueab@gmail.com>
2023-01-11 21:56:46 -0300

    Adicionando component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { StatusBar } from 'expo-status-bar';
@@ -11,6 +12,8 @@ export function Groups() {
         title='Turmas'
         subtitle='Jogue com sua turma'
       />
+
+      <GroupCard title='Galera da zueira' />
     </Container>
   );
 }
