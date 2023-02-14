75c17f7f049656daf1d3515a373c0fde0ce120e2
Ryan Henrique de Assis Belfort <ryanhenriqueab@gmail.com>
2023-02-14 08:42:38 -0300

    Adicionando o o componente ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
+import { FlatList } from 'react-native';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { StatusBar } from 'expo-status-bar';
 import { Container } from './styles';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header showBackButton/>
@@ -13,7 +20,17 @@ export function Groups() {
         subtitle='Jogue com sua turma'
       />
 
-      <GroupCard title='Galera da zueira' />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+        // contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => <ListEmpty  message='Cadastre a sua primeira turma'/>}
+      />
     </Container>
   );
 }
